f1edd265099bf93b3efcdcefed8020ef21c9db57
Martin Pedersen <martin@m4r7.in>
2023-07-20 20:31:22 +0000

    use newer Wikipedia API

--- src/download_wikipedia_article.js
+++ src/download_wikipedia_article.js
@@ -7,11 +7,17 @@ import rehypeStringify from 'rehype-stringify';
 import { filter } from 'unist-util-filter';
 
 /**
+ * @typedef {{
+ *  parse: { title: string; pageid: number; text: { "*": string; }; };
+ * }} WikipediaResponse
+ */
+
+/**
  * @param {string} title
  * @returns {Promise<string>}
  */
 export default async function downloadWikipediaArticle(title) {
-    const url = `https://no.wikipedia.org/api/rest_v1/page/mobile-sections/${encodeURIComponent(
+    const url = `https://no.wikipedia.org/w/api.php?action=parse&format=json&prop=text&page=${encodeURIComponent(
         title.replace(' ', '_')
     )}`;
     const res = await fetch(url, {
@@ -20,50 +26,34 @@ export default async function downloadWikipediaArticle(title) {
         },
     });
 
+    if (!res.ok) {
+        throw new Error(
+            `Failed to download Wikipedia article: ${res.status} ${res.statusText}`
+        );
+    }
+
+    /** @type {WikipediaResponse} */
     const data = await res.json();
-    const html =
-        `<h1>${data.lead.displaytitle}</h1>` +
-        data.lead.sections
-            .map((leadSection) => {
-                let text = leadSection.text;
-                if (!text) {
-                    const remainingSection = data.remaining.sections.find(
-                        (remainingSection) =>
-                            remainingSection.id === leadSection.id
-                    );
-                    if (
-                        remainingSection.anchor === 'Eksterne_lenker' ||
-                        remainingSection.anchor === 'Referanser'
-                    ) {
-                        return '';
-                    }
-                    text =
-                        '<h2>' +
-                        remainingSection.line +
-                        '</h2>' +
-                        '\n' +
-                        remainingSection.text;
-                }
-                return text.trim();
-            })
-            .join('\n\n');
+    const html = `<h1>${data.parse.title}</h1>${data.parse.text['*']}`;
 
     const file = await unified()
         .use(rehypeParse, { fragment: true })
         .use(
             () => (tree) =>
-                filter(
-                    tree,
-                    { cascade: false },
-                    (node) =>
+                filter(tree, { cascade: false }, (node) => {
+                    const { tagName } = node;
+                    const { className = '', id = '' } = node.properties || {};
+                    return (
+                        !className.includes('noexcerpt') &&
+                        !className.includes('reference') &&
+                        !className.includes('mw-editsection') &&
+                        id != 'toc' &&
                         !(
-                            node.tagName === 'table' &&
-                            node.properties.className?.includes?.('infobox')
+                            tagName === 'table' && className.includes('infobox')
                         ) &&
-                        !node.properties?.className?.includes?.(
-                            'mw-references-wrap'
-                        )
-                )
+                        !className.includes('mw-references-wrap')
+                    );
+                })
         )
         .use(rehypeSanitize, {
             ...defaultSchema,
